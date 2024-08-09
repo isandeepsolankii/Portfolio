7e7443e44451b2c3b28b1f949c1231d062391ce8
Sandeep Solanki <59826981+isandeepsolankii@users.noreply.github.com>
2024-08-09 22:22:02 +0530

    made changes to form

--- assets/php/send_email.php
+++ assets/php/send_email.php
@@ -2,13 +2,11 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['full_name'];
     $email = $_POST['email_address'];
-    $phone = $_POST['phone_number'];
-    $location = $_POST['property_location'];
     $comments = $_POST['other_comments'];
 
     $to = "sandeepsolanki672s@gmail.com"; // Replace with your email address
     $subject = "Safe Homes New Message from Client";
-    $message = "Full Name: $name\nEmail: $email\nPhone Number: $phone\nLocation of Property: $location\nComments: $comments";
+    $message = "Full Name: $name\nEmail: $email\nComments: $comments";
 
     $headers = "From: $email";
 
